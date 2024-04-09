b6c26e401f720f727c90d69e64d8f34498ca5c55
mmj030703 <mayankjain7738@gmail.com>
2024-04-10 02:43:05 +0530

    Lazy loading added to the images

--- src/components/LearningPathRecommendations.jsx
+++ src/components/LearningPathRecommendations.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function LearningPathRecommendations() {
   const [selectedOption, setSelectedOption] = useState(null);
@@ -7,7 +9,7 @@ function LearningPathRecommendations() {
   };
 
   return (
-    <div className="mx-auto px-5 flex flex-col justify-center items-center mt-36">
+    <div className="mx-auto px-5 flex flex-col justify-center items-center mt-10 sm:mt-36">
       <div className="text-center">
         <h1 className="text-[1.4rem] xs:text-[1.7rem] sm:text-[2rem] md:text-[2.5rem] text-slate-900 font-bold">Learning paths based on your answers</h1>
         <p className="text-[15px] xs:text-[16px] sm:text-[16.5px] mt-5 text-slate-700">Choose one to get started. You can switch anytime.</p>
@@ -18,14 +20,14 @@ function LearningPathRecommendations() {
           onClick={() => handleSelectOption(1)}
         >
           <p className="w-[200px] font-bold text-[16.3px]">Foundational Math <span className="font-normal">Build your foundational skills in algebra, geometry, and probability.</span></p>
-          <img src="https://images.unsplash.com/photo-1698993081947-8a3654303904?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Student Image" className="w-36 h-24 object-cover" />
+          <LazyLoadImage src="https://images.unsplash.com/photo-1518133910546-b6c2fb7d79e3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image" className="w-36 h-24 object-cover" effect="blur" />
           <p className="absolute -top-3 left-[60px] xs:min-[460px]:left-[135px] bg-amber-300 text-[13px] font-medium py-[3px] px-4 rounded-2xl">Most Popular</p>
         </div>
         <div className={`relative pt-8 pb-6 hover:shadow-md hover:border-[1px] transition-all duration-200 hover:border-gray-400 flex gap-y-5 flex-col xs:min-[460px]:flex-row items-center  gap-x-3 mx-2 cursor-pointer rounded-[5px] border border-gray-200 p-4 ${selectedOption === 2 ? "border-[1px] border-gray-400 shadow-md animate-scale" : ""}`}
           onClick={() => handleSelectOption(2)}
         >
           <p className="w-[200px] font-bold text-[16.3px]">Mathematical Thinking <span className="font-normal">Build your foundational skills in algebra, geometry, and probability.</span></p>
-          <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Professional Image" className="w-36 h-24 object-cover" />
+          <LazyLoadImage src="https://plus.unsplash.com/premium_photo-1683134169138-9037062cba51?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image" className="w-36 h-24 object-cover" effect="blur" />
         </div>
       </div>
     </div>
